9d97c824fe2f4f318144af25d2bb6e409e5a1d2e
Emrys Shevek <emrysshevek@gmail.com>
2025-04-23 17:51:28 -0700

    checkpoint

--- scripts/minigames/load/selectable.gd
+++ scripts/minigames/load/selectable.gd
@@ -3,10 +3,10 @@ extends ItemState
 
 func enter(_previous_state_path: String, _data := {}) -> void:
 	item.selected.connect(_on_item_selected)
-	
+
 func exit() -> void:
 	item.selected.disconnect(_on_item_selected)
-		
+
 func _on_item_selected(item: GameItem) -> void:
 	#item.selected.emit(item)
 	finished.emit(CLICK)
