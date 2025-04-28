adeb0358f454104fa51dfef0b7a2d6fcc6b68060
Emrys Shevek <emrysshevek@gmail.com>
2025-04-28 15:42:39 -0700

    darkroom environment model kind of working

--- scripts/entities/interactables/test_object/test_object.gd
+++ scripts/entities/interactables/test_object/test_object.gd
@@ -1,12 +1,12 @@
 class_name TestObject extends InteractableObject
 
-var lock_minigame_scene = preload("res://scripts/minigames/film_dev/lock_minigame.tscn")
+#var lock_minigame_scene = preload("res://scripts/minigames/film_dev/lock_minigame.tscn")
 var minigames: Array = []
 func interact():
 	super.interact()
-	var lock_minigame_instance = lock_minigame_scene.instantiate()
-	$MiniGameScreen.add_child(lock_minigame_instance)
-	minigames.append(lock_minigame_instance)
+	#var lock_minigame_instance = lock_minigame_scene.instantiate()
+	#$MiniGameScreen.add_child(lock_minigame_instance)
+	#minigames.append(lock_minigame_instance)
 
 func _process(_delta):
 	if all_minigames_completed():
