a0c44265d5d25bdc2bbc43e1df8284a6194a2534
Emrys Shevek <emrysshevek@gmail.com>
2025-05-09 13:07:21 -0700

    small tweaks

--- scripts/rooms/darkroom/darkroom.gd
+++ scripts/rooms/darkroom/darkroom.gd
@@ -4,13 +4,13 @@ class_name Darkroom extends Node3D
 
 var curr_camera_idx := 0
 
-func _ready() -> void:
-	cameras[curr_camera_idx].make_current()
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("left"):
-		curr_camera_idx = (curr_camera_idx + 1) % len(cameras)
-	elif Input.is_action_just_pressed("right"):
-		curr_camera_idx = (curr_camera_idx - 1) % len(cameras)
-		
-	cameras[curr_camera_idx].make_current()
+#func _ready() -> void:
+	#cameras[curr_camera_idx].make_current()
+#
+#func _physics_process(delta: float) -> void:
+	#if Input.is_action_just_pressed("left"):
+		#curr_camera_idx = (curr_camera_idx + 1) % len(cameras)
+	#elif Input.is_action_just_pressed("right"):
+		#curr_camera_idx = (curr_camera_idx - 1) % len(cameras)
+		#
+	#cameras[curr_camera_idx].make_current()
