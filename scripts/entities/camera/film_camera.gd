d88e4f5db684cfec2a2ec6b2c44028a3c261fc17
Emrys Shevek <emrysshevek@gmail.com>
2025-05-15 17:46:35 -0700

    basic focusing works!

--- scripts/entities/camera/film_camera.gd
+++ scripts/entities/camera/film_camera.gd
@@ -2,19 +2,20 @@ extends Camera3D
 
 @export var focus_dist := 1.0
 @export var focus_range := Vector2(0.01, 1000.0)
-@export var focus_step := 1.05
+@export var focus_step := .1
 
 @onready var focus_shader: ShaderMaterial = $FocusScreen.material_override
 
 func _ready() -> void:
-	focus_shader.set_shader_parameter("focus_dist", focus_dist)
+	focus_shader.set_shader_parameter("u", focus_dist)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				focus_dist = clamp(focus_dist * focus_step, focus_range.x, focus_range.y)
+					
+				focus_dist = clamp(focus_dist + focus_step, focus_range.x, focus_range.y)
 			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				focus_dist = clamp(focus_dist / focus_step, focus_range.x, focus_range.y)
+				focus_dist = clamp(focus_dist - focus_step, focus_range.x, focus_range.y)
 			print("updated focus dist: ", focus_dist)
-			focus_shader.set_shader_parameter("focus_dist", focus_dist)
+			focus_shader.set_shader_parameter("u", focus_dist)
