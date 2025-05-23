da477b8e54e5c1065aedd3644b614aeebed0c5d4
Emrys Shevek <emrysshevek@gmail.com>
2025-05-23 10:32:25 -0700

    everything combined

--- scripts/entities/print/print.gd
+++ scripts/entities/print/print.gd
@@ -1,19 +1,30 @@
-extends Control
+extends Sprite2D
 class_name Print
 
-@export var negative: Texture2D
+var developing := false
+var developed := false
 
-@onready var image: TextureRect = $PaperArea/ImageArea
-@onready var dev_fader: ShaderMaterial = $PaperArea/DevFadeShader.material
-
-func _ready() -> void:
-	image.texture = negative
+var fade := 0.0
+var strength := 0.0
 	
-#func _process(_delta: float) -> void:
-	#if (Input.is_action_just_pressed(&"interact")):
-		#print("in print")
-		#create_tween().tween_method(set_developer, 0.0, 1.0, 10)
-
-func set_developer(val: float) -> void:
-	dev_fader.set_shader_parameter("fade", val)
+func _process(_delta: float) -> void:
+	print("fade: ", fade, ", strength: ", strength)
+	material.set_shader_parameter("fade", fade)
+	material.set_shader_parameter("strength", strength)
+		
+func develop(time:=20.0) -> void:
+	if not developed and not developing:
+		developing = true
+		fade = 0.0
+		strength = 0.0
+		var tween = create_tween()
+		tween.set_parallel(true)
+		tween.tween_property(self, ^"fade", 1.0, time)
+		var strength_tween = tween.tween_property(self, ^"strength", 1.0, time)
+		strength_tween.set_trans(Tween.TRANS_CUBIC)
+		strength_tween.set_ease(Tween.EASE_IN_OUT)
+		tween.finished.connect(_on_finished_development)
 	
+func _on_finished_development() -> void:
+	developed = true
+	developing = false
--- scripts/minigames/paper_dev/paper_dev_minigame.gd
+++ scripts/minigames/paper_dev/paper_dev_minigame.gd
@@ -1,12 +1,56 @@
 extends Node2D
 class_name PaperDevMinigame
 
-@onready var tray: Tray = $CanvasLayer/Tray
+@export var development_time := 20.0
+@export var wave_travel_time := 2.0
+@export var tilt_duration := .75
+
+var tilt := 0.0
+var wave_pos := 0.0
+var distortion := 0.0
+var max_distortion := 25.0
+
+@onready var tray_shader_mat: ShaderMaterial = $CanvasLayer2/TrayShader.material
+@onready var print: Print = $Print
+@onready var tween: Tween
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"interact"):
-		tray.rock_tray()
+		rock_tray()
+		print.develop(development_time)
 		
 
+func _process(_delta: float) -> void:
+	#print(tilt, " ", wave_pos, " ", distortion)
+	update_tray()
 	
+func update_tray() -> void:
+	tray_shader_mat.set_shader_parameter("tilt", tilt)
+	tray_shader_mat.set_shader_parameter("wave_pos", wave_pos)
+	tray_shader_mat.set_shader_parameter("distortion", distortion)
 	
+func rock_tray() -> void:
+	print("Rocking tray")
+	if tween != null:
+		tween.kill()
+	
+	tilt = 0.0
+	tween = create_tween()
+	
+	tween.set_parallel(true)
+	tween.tween_property(self, ^"distortion", 0.0, tilt_duration / 2)
+	var tilt_up = tween.tween_property(self, ^"tilt", 1.0, tilt_duration / 2)
+	tilt_up.set_trans(Tween.TRANS_QUINT)
+	tilt_up.set_ease(Tween.EASE_IN_OUT)
+	
+	tween.set_parallel(false)
+	var tilt_down = tween.tween_property(self, ^"tilt", 0.0, tilt_duration / 2)
+	tilt_down.set_trans(Tween.TRANS_QUINT)
+	tilt_down.set_ease(Tween.EASE_IN_OUT)
+	
+	wave_pos = .84
+	tween.set_parallel(true)
+	for i in range(8):
+		var delay = i * wave_travel_time
+		tween.tween_property(self, ^"wave_pos", .1 if i % 2 == 0 else .9, wave_travel_time).set_delay(delay)
+		tween.tween_property(self, ^"distortion", max_distortion * pow(.5, i), wave_travel_time).set_delay(delay)
